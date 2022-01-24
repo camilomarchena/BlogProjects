9fcde46975b9da3258b9489b0ba9d3f470411579
Camilo <camilormarchena@gmail.com>
2022-01-24 16:54:29 -0500

    edit local.py

--- blog/blog/settings/local.py
+++ blog/blog/settings/local.py
@@ -33,7 +33,7 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.joing(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.child('static')]
 django_heroku.settings(locals())
