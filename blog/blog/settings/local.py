b2c70f35eebf4512a30b5ad032c935794324b54d
Camilo <camilormarchena@gmail.com>
2022-01-24 15:54:58 -0500

    deploy heroku

--- blog/blog/settings/local.py
+++ blog/blog/settings/local.py
@@ -1,27 +1,43 @@
 from .base import *
+import os
+import django_heroku
+import django
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-blog-projects.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': get_secret('DB_NAME'),
+#         'USER': get_secret('USER'),
+#         'PASSWORD': get_secret('PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '5433',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_secret('DB_NAME'),
-        'USER': get_secret('USER'),
-        'PASSWORD': get_secret('PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5433',
+        'NAME': 'dbm1auhevc7ocm',
+        'USER': 'uevuzpwpnrcrfu',
+        'PASSWORD': '2f04efabcfcd98635fa720aa70875dc55e335103204e96503ebe2474db810928',
+        'HOST': 'ec2-54-208-139-247.compute-1.amazonaws.com ',
+        'PORT': '5432',
     }
 }
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.joing(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.child('static')]
+django.heroku.settings(locals())
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.child('media')
